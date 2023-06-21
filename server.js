91c8d4787ba8c74e2aef245aebc4b7053316fcee
Axel Tokam <mail.axeltokam@gmail.com>
2023-06-21 12:50:05 +0100

    Minor bugs correction for deployment
    Just made some minor corrections for the app to work on vercel

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // Set up static files directory
-app.use("/assets", express.static('public'));
+app.use("/assets", express.static(path.join(__dirname, 'public')));
 
 // Set Middlewares 
 app.use(bodyparser.urlencoded({ extended:false }));
